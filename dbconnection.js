2abefede4f51c6ee0e8c6a29c4eccb2bb3b65829
Dharani4432 <dharanireddy952@gmail.com>
2024-04-23 17:29:28 +1000

    initial commit

---
+++ dbconnection.js
@@ -0,0 +1,28 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://s224092776:Dharnani12345@cluster0.cuthsbx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function connectDatabase() {
+    try {
+        // Connect the client to the server
+        await client.connect();
+        console.log("Connected to MongoDB!");
+        // Ping the deployment to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged deployment. Successfully connected to MongoDB!");
+        return client;
+    } catch (error) {
+        console.error('Failed to connect to MongoDB:', error);
+        throw error;
+    }
+}
+
+module.exports = { connectDatabase };
